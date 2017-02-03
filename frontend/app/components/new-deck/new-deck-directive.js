e87231c67666836686522e0c39885eeb02a7460b
Parul Gupta <parul.gupta@team.telstra.com>
2017-02-03 20:33:31 +0530

    master validations for create section and deck

--- frontend/app/components/new-deck/new-deck-directive.js
+++ frontend/app/components/new-deck/new-deck-directive.js
@@ -1,12 +1,25 @@
 'use strict';
 
-function DeckController() {
+function DeckController($http) {
   let vm = this;
   vm.industries = ['TMT', 'CNIP', 'others'];
-  vm.industryVal = vm.industries[0];
+  vm.industry = vm.industries[0];
+  vm.isDeckCreated = 'no_alert';
+
+  vm.showError = function(input) {
+    return (vm.formCreateDeck.$submitted && vm.formCreateDeck[input].$invalid);
+  }
 
   vm.submitDeck = function() {
-    alert('HI');
+    let data = {
+      name: vm.name,
+      industry: vm.industry
+    }
+    $http.post('http://10.118.37.64:4000/admin/deck', data).then(function(res) {
+      vm.isDeckCreated = 'success';
+    }, function(err) {
+      vm.isDeckCreated = 'error';
+    });
   }
 }
 
--- frontend/app/components/new-section/new-section-directive.js
+++ frontend/app/components/new-section/new-section-directive.js
@@ -1,7 +1,23 @@
 'use strict';
 
-function NewSectionController() {
+function NewSectionController($http) {
+  vm.isSectionCreated = 'no_alert';
 
+  vm.showError = function(input) {
+    return (vm.formCreateSection.$submitted && vm.formCreateSection[input].$invalid);
+  }
+
+  vm.submitDeck = function() {
+    let data = {
+      name: vm.name,
+      industry: vm.industry
+    }
+    $http.post('http://10.118.37.64:4000/admin/deck', data).then(function(res) {
+      vm.isDeckCreated = 'success';
+    }, function(err) {
+      vm.isDeckCreated = 'error';
+    });
+  }
 }
 
 angular.module('myApp.new-section-directive', [])
