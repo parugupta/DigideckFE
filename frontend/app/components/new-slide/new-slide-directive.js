8cc3f59ea92c114e078f039fc2d7b0b2a29bea8e
Parul Gupta <parul.gupta@team.telstra.com>
2017-02-03 20:04:33 +0530

    digideck-cleanup validations added

--- frontend/app/components/new-slide/new-slide-directive.js
+++ frontend/app/components/new-slide/new-slide-directive.js
@@ -2,6 +2,11 @@
 
 function NewSlideController($http) {
   let vm = this;
+  vm.isSlideCreated = 'no_alert';
+
+  vm.showError = function(input) {
+    return (vm.formCreateSlide.$submitted && vm.formCreateSlide[input].$invalid);
+  }
   
   vm.submitSlide = function() {
     let data = {
@@ -10,10 +15,9 @@ function NewSlideController($http) {
       video_url: vm.videoUrl
     }
     $http.post('http://10.118.37.64:4000/admin/slide', data).then(function(res) {
-      alert('SUCCESS', res);
+      vm.isSlideCreated = 'success';
     }, function(err) {
-      console.log(data);
-      alert('FAIL', err);
+      vm.isSlideCreated = 'error';
     });
   }
 }
